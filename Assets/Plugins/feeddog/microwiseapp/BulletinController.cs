5da6bcd9bed5424548ac93809e55d91507061f23
do-nothing <lj110119@aliyun.com>
2017-07-23 11:43:34 +0800

    add show bulletin logic.

---
+++ Assets/Plugins/feeddog/microwiseapp/BulletinController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using LitJson;
+
+public class BulletinController : MonoBehaviour
+{
+    void Start()
+    {
+        AppInfo.instance.conn.addMessageListener(processMessage);
+    }
+
+    private void processMessage(JsonData json)
+    {
+        if (json["monitorId"] == null)
+        {
+            json["monitorId"] = "";
+        }
+        //Debug.Log(json.ToJson());
+        if(json["contentBean"]["command"].ToString() == "showBulletin")
+        {
+            showMessage(json["contentBean"]["args"][0].ToString());
+        }
+    }
+
+    private void showMessage(string str)
+    {
+        Debug.Log(str);
+    }
+}
--- Assets/Plugins/feeddog/netController/udpconn/UdpMessenger.cs
+++ Assets/Plugins/feeddog/netController/udpconn/UdpMessenger.cs
@@ -53,13 +53,13 @@ namespace Microwise.Guide.NetConn
         {
             json["timestamp"] = GetTimeStamp();
             string str = json.ToJson();
-            byte[] data = Encoding.ASCII.GetBytes(str);
+            byte[] data = Encoding.UTF8.GetBytes(str);
             server.SendTo(data, data.Length, SocketFlags.None, ipEndPoint);
 
-            if (json["strategy"].ToString() != "heatbeat")
+            if (json["strategy"].ToString() != "heartbeat")
             {
+                Debug.Log(str);
             }
-            //Debug.Log(str);
         }
 
         public void addMessageListener(ProcessMessage processMessage)
@@ -100,7 +100,7 @@ namespace Microwise.Guide.NetConn
                 EndPoint ep = new IPEndPoint(IPAddress.Any, 0); ;
 
                 int intReceiveLength = server.ReceiveFrom(data, ref ep);
-                string strReceiveStr = Encoding.Default.GetString(data, 0, intReceiveLength);
+                string strReceiveStr = Encoding.UTF8.GetString(data, 0, intReceiveLength);
                 //Debug.Log(strReceiveStr);
                 JsonData json = JsonMapper.ToObject(strReceiveStr);
                 processMessage(json);
